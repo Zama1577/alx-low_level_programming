fb065a4016ccd2c23d8c2713fe3683fb8036de47
Zama1577 <phangas.jnr@gmail.com>
2023-02-21 14:01:59 -0800

    Getting to know C

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,19 @@
+
 #include "main.h"
 
 /**
- * pint_last_digit- prints the last digit of number
- * Return: the last digit
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int print_last_digit(int n)
+int main(void)
 {
-    int last_dit = n % 10;
+    int r;
 
-    if (n < 0)
-    {
-        last_dit = last_dit * -1;
-    }
-    _putchar(last_dit + '\0');
-    return (last_dit);
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
 }
