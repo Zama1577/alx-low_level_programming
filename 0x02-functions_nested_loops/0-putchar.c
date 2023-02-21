51ce718219da794cb418ab53620d92c8af5a8de4
Zama1577 <phangas.jnr@gmail.com>
2023-02-21 13:33:28 -0800

    Getting to know C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- *
+ * main - prints the word _putchar
+ * Return: return 0
  */
 
 int main(void)
 {
-	_putchar('m');
-	_putchar('a');
-	_putchar('i');
-	_putchar('n');
-	_putchar('\n');
+	char word[9] = "_putchar";
+	int i = 0;
 
+	while (i < 8)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
