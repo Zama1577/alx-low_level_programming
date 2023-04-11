1dd7f5f0d98b10dee6e07039fa069d44c0d35e29
Zama1577 <phangas.jnr@gmail.com>
2023-04-11 14:14:51 -0700

    bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - create a function that prints the binary representation of a number.
+ * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
  */
 
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * set_bit - create a function that sets the value of a bit to 1 at a given index
+ * set_bit - sets the value of a bit at a given index
  * @n: pointer to the number to modify
- * @index: starting from "0" of the bit you want to set
- * Return: "1" if successful, "-1" if an error occurs
+ * @index: index of the bit to set
+ * Return: 1 if successful, -1 if an error occurs
  **/
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
